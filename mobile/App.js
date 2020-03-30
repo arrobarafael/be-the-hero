3ba7b114ff058073673ae5393f967551622a4c26
Rafael Pelegrini <arrobarafael@hotmail.com>
2020-03-30 00:39:03 -0300

    Mobile da aplicação

---
+++ mobile/App.js
@@ -0,0 +1,16 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import Routes from './src/routes';
+// import Incidents from './src/pages/Incidents';
+// import Detail from './src/pages/Detail';
+
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
