f7105bc0cbf27653efaada540aa1054cb4696833
Rafael Pelegrini <arrobarafael@hotmail.com>
2020-09-28 15:24:45 -0300

    Teste

--- backend/src/controllers/IncindentController.js
+++ backend/src/controllers/IncindentController.js
@@ -43,7 +43,7 @@ module.exports = {
     async delete(request, response){
         const { id } = request.params
         const ong_id = request.headers.authorization
-
+ 
         const incident = await connection('incidents')
             .where('id', id)
             .select('ong_id')
